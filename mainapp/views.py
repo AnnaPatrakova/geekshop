0cd9c633f7e59446b318f571248dc274d0f6656a
annapatrakova <anna.patrakova@gmail.com>
2021-04-26 00:50:52 +0200

    added context for controllers

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 
 
-
-# Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {'title': 'GeekShop-Каталог'}
+    return render(request, 'mainapp/products.html', context)
 
 
 
