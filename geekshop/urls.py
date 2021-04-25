6a320535767750e4c4d97e988d475d4c0b414e5b
annapatrakova <anna.patrakova@gmail.com>
2021-04-25 21:53:21 +0200

    added correct work with product a with static

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import index
+from mainapp.views import index, products
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('products/', products, name='products'),
+
+
 ]
