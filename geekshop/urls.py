19a058052b130d3ae65b9d800d31e6c3cef9237b
annapatrakova <anna.patrakova@gmail.com>
2021-04-29 00:55:41 +0200

    added media files

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp.views import index, products
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -26,3 +28,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
