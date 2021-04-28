a03ca855fb3875d11aed3d05393697af9d8de137
annapatrakova <anna.patrakova@gmail.com>
2021-04-29 00:19:53 +0200

    added models in admin panel (there are problems with def __str__)

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,6 +5,12 @@ class ProductCategory(models.Model):
    name = models.CharField(max_length=64, unique=True)
    description = models.TextField(blank=True)
 
+   class Meta:
+       verbose_name_plural = 'Product Categories'
+
+    # def __str__(self):
+    #     return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images', blank=True)
@@ -12,3 +18,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return f'{self.name}|{self.category.name}'
