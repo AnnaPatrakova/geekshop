363b1ef9916fee27de309c397e38c3e93d6667f5
annapatrakova <anna.patrakova@gmail.com>
2021-04-29 10:04:57 +0200

    added url namespaces

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainapp.views import index, products
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,9 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
-
-
+    path('products/', include('mainapp.urls', namespace='products')),
 ]
 
 if settings.DEBUG:
