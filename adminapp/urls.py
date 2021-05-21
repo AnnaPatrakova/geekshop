17154cf80ba9a1626a9803560cb3c424a43c19a6
annapatrakova <anna.patrakova@gmail.com>
2021-05-21 23:50:50 +0200

    added admin-users-create

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from adminapp.views import index, admin_users_read
+from adminapp.views import index, admin_users_read, admin_users_create
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('admin-users-read', admin_users_read, name='admin_users_read'),
+    path('admin-users-read/', admin_users_read, name='admin_users_read'),
+    path('admin-users-create/', admin_users_create, name='admin_users_create'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -9,5 +9,8 @@ def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
+def admin_users_create(request):
+    return render(request, 'adminapp/admin-users-create.html')
+
 
 # Create your views here.
