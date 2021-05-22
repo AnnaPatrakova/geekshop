bd66fcd112387298347d0a8ba24e940aa5c0831a
annapatrakova <anna.patrakova@gmail.com>
2021-05-22 19:53:06 +0200

    added create users

---
+++ adminapp/forms.py
@@ -0,0 +1,10 @@
+from authapp.forms import UserRegisterForm
+from authapp.models import User
+from django import forms
+
+class UserAdminRegisterForm(UserRegisterForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password2')
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
 
 def index(request):
     return render(request, 'adminapp/admin.html')
@@ -10,7 +12,17 @@ def admin_users_read(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 def admin_users_create(request):
-    return render(request, 'adminapp/admin-users-create.html')
+        if request.method == 'POST':
+            form = UserAdminRegisterForm(data=request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+        else:
+            form = UserAdminRegisterForm()
+        context = {'form': form}
+        return render(request, 'adminapp/admin-users-create.html', context)
+
+
 
 
 # Create your views here.
