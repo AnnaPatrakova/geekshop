407f992db03424c6886eed5cd03396d7342c05f2
annapatrakova <anna.patrakova@gmail.com>
2021-05-24 11:03:55 +0200

    added decorator for checking permissions

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/admin.html')
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
         if request.method == 'POST':
             form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
@@ -22,6 +26,7 @@ def admin_users_create(request):
         context = {'form': form}
         return render(request, 'adminapp/admin-users-create.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     selected_user = User.objects.get(id=user_id)
     if request.method == 'POST':
@@ -34,6 +39,7 @@ def admin_users_update(request, user_id):
     context = {'form': form, 'selected_user': selected_user}
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_remove(request, user_id):
     user = User.objects.get(id=user_id)
     user.is_active = False
