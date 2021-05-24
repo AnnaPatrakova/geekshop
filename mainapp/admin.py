f2dbd0306cdd9998241ba21434beea2ea436dc2a
annapatrakova <anna.patrakova@gmail.com>
2021-05-24 12:39:40 +0200

    changed admin panel django

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from mainapp.models import ProductCategory, Product
 
-admin.site.register(Product)
+
 admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    ordering = ('price',)
+    search_fields = ('name',)
