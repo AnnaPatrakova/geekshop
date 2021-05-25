9f88e2f3711c11c8a83210f55cfbec5936e8b93a
annapatrakova <anna.patrakova@gmail.com>
2021-05-25 10:25:03 +0200

    added paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', products, name='index'),
     path('<int:category_id>/', products, name='product'),
+    path('page/<int:page>/', products, name='page'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-        context.update({'products': products})
-    else:
-        context.update({'products': Product.objects.all()})
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context.update({'products': products_paginator})
     return render(request, 'mainapp/products.html', context)
 
 
